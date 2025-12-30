ca324f9a6431a735760ca60dd2e258cf9eaf5a12
aibayanyu20 <aibayanyu@qq.com>
2025-12-30 16:00:26 +0800

    chore: update docs

--- playground/src/pages/components/qr-code/demo/base.vue
+++ playground/src/pages/components/qr-code/demo/base.vue
@@ -7,6 +7,6 @@ const text = ref('https://www.antdv-next.com')
 <template>
   <a-space direction="vertical" align="center">
     <a-qrcode :value="text || '-'" />
-    <input v-model="text" placeholder="-" maxlength="60">
+    <a-input v-model:value="text" placeholder="-" :maxlength="60" />
   </a-space>
 </template>
